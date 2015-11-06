5e61463bc4d56f877499a8df6353b933822fea36
Christian <me@rndm.de>
2015-11-06 19:53:34 +0100

    replaces uncredited ascii flamingo with custom build image

--- src/routes/index.js
+++ src/routes/index.js
@@ -6,26 +6,14 @@ var pkg = require('../../package');
  * @module flamingo/src/routes/index
  */
 
-var BANNER = [
-  '<!doctype html><html lang=""><head><title>' + pkg.name + '@' + pkg.version + '</title><style>\n  html{\n    display: table;\n    width: 100%;\n    height: 100%;\n  }\n  body{\n    display: table-cell;\n    vertical-align: middle;\n    text-align: center;\n    color: #222;\n    font-family: "Fira Sans", sans-serif;\n  }\n  h1{\n    font-weight: normal;\n  }\n  a{\n    color: #666;\n  }\n  pre{\n    display: inline-block;\n    text-align: left;\n    font-size: 20px;\n  }\n</style></head><body>',
-  '<pre>',
-  ' .-.',
-  '(-`))',
-  '  //',
-  ' //',
-  '((_.="""=.',
-  ' \'.   ,.  \'',
-  '   \'-._,)__\'',
-  '      |\\   `',
-  '    __|_\'',
-  '  ((` |',
-  '      |',
-  '    =="-',
-  '',
-  '</pre><h1>',
-  pkg.name + '@' + pkg.version,
-  '</h1><p><a href="' + pkg.repository.url + '">' + pkg.repository.url + '</a></p>',
-  '</body></html>'].join('\n');
+var BASE64_ICON = 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAYAAAAf8/9hAAAAuElEQVQ4jbWRMQqDQBBFnyGVSJpUkj72e4/UphUP4FlyAEkteIecIPZeQAlYiW67qTagzKIo+eWy78+fP/Av9Ulm+iQzS/88F9zqAYDQDzg9H+I/gIP0+O4+REXuNXrEGm1SHaeLK+yGxRXqODWNHoHlMsVy6jg1oR8ATDqQChUTWHAOt3pgnkY0iIp8MsXCm1QqZUqltl/Dwi6Towt83e4/oARzvlyhqtZPtga74s+TSHKecQ0M8AXIOlSRGTKPKQAAAABJRU5ErkJggg==',
+  BANNER = [
+    '<!doctype html><html lang=""><head><link rel="icon" href="' + BASE64_ICON + '"><title>' + pkg.name + '@' + pkg.version + '</title><meta name="viewport" content="width=device-width, initial-scale=1"><style>\n  html{\n    display: table;\n    width: 100%;\n    height: 100%;\n  }\n  body{\n    display: table-cell;\n    vertical-align: middle;\n    text-align: center;\n    color: #222;\n    font-family: "Fira Sans", sans-serif;\n  }\n  h1{\n    font-weight: normal;\n  }\n  a{\n    color: #666;\n  }\n  img{\n    width: 200px;\n    image-rendering: -moz-crisp-edges;\n    image-rendering: -o-crisp-edges;\n    image-rendering: -webkit-optimize-contrast;\n    -ms-interpolation-mode: nearest-neighbor;\n    image-rendering: pixelated;\n  }\n</style></head><body>',
+    '<img src="' + BASE64_ICON + '" alt=""/>',
+    '<h1>',
+    pkg.name + '@' + pkg.version,
+    '</h1><p><a href="' + pkg.repository.url + '">' + pkg.repository.url + '</a></p>',
+    '</body></html>'].join('\n');
 
 /**
  * Function to generate the index route hapi configuration
