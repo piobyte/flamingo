c49e2006766fe119f460c6b69b233578d2499ba8
Christian <me@rndm.de>
2015-11-19 12:47:18 +0100

    removes im gpg check

--- tools/install-imagemagick.sh
+++ tools/install-imagemagick.sh
@@ -7,10 +7,10 @@ ARCHIVE="http://www.imagemagick.org/download/ImageMagick.tar.gz"
 
 cd /tmp/
 wget ${ARCHIVE}
-wget ${ARCHIVE}.asc
+#wget ${ARCHIVE}.asc
 
-gpg --keyserver pgpkeys.mit.edu --recv-key 8277377A
-gpg ImageMagick.tar.gz.asc
+#gpg --keyserver pgpkeys.mit.edu --recv-key 8277377A
+#gpg ImageMagick.tar.gz.asc
 
 tar zxf ImageMagick.tar.gz
 cd ImageMagick-*
